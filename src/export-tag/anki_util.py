6b447137088a526a260d01933d0ebe799e0f1579
RisingOrange <jakub.fidler@protonmail.com>
2021-08-20 15:37:19 +0200

    Remove not needed files and code

--- src/export-tag/anki_util.py
+++ src/export-tag/anki_util.py
@@ -1,14 +1,6 @@
 from aqt import mw
 
 
-
-def get_notes(search_string):
-    return [
-        mw.col.getNote(id)
-        for id in mw.col.find_notes(search_string)
-    ]
-
-
 def all_tags():
     return _all_partial_paths(mw.col.tags.all(), '::')
 
